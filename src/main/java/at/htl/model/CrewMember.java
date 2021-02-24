782aa8c909ce76733b5bf3480c92e3f149ac3741
FitzingerJ <jacki.fitzinger@gmail.com>
2021-02-24 21:10:06 +0100

    #7 CrewMember Entity finished

---
+++ src/main/java/at/htl/model/CrewMember.java
@@ -0,0 +1,74 @@
+package java.at.htl.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CREWMEMBER")
+public class CrewMember {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CM_ID")
+    Long id;
+
+    @Column(name = "CM_DESC")
+    String desc;
+
+    @ManyToOne
+    @JoinColumn(name = "CM_T")
+    Tour tour;
+
+    @ManyToOne
+    @JoinColumn(name = "CM_P")
+    Person person;
+
+    public CrewMember() {
+    }
+
+    public CrewMember(String desc, Tour tour, Person person) {
+        this.desc = desc;
+        this.tour = tour;
+        this.person = person;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Tour getTour() {
+        return tour;
+    }
+
+    public void setTour(Tour tour) {
+        this.tour = tour;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "CrewMember{" +
+                "id=" + id +
+                ", desc='" + desc + '\'' +
+                ", tour=" + tour +
+                ", person=" + person +
+                '}';
+    }
+}

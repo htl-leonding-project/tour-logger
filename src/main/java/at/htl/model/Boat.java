8e0b502e98f8f28f725d4ed0f6fda989c2cc6595
FitzingerJ <jacki.fitzinger@gmail.com>
2021-02-24 21:02:54 +0100

    #7 Boat Entity finished

---
+++ src/main/java/at/htl/model/Boat.java
@@ -0,0 +1,60 @@
+package java.at.htl.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BOAT")
+public class Boat {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "B_ID")
+    Long id;
+
+    @Column(name = "B_NAME")
+    String name;
+
+    @ManyToOne
+    @JoinColumn(name = "B_BT")
+    BoatType boatType;
+
+    public Boat() {
+    }
+
+    public Boat(String name, BoatType boatType) {
+        this.name = name;
+        this.boatType = boatType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BoatType getBoatType() {
+        return boatType;
+    }
+
+    public void setBoatType(BoatType boatType) {
+        this.boatType = boatType;
+    }
+
+    @Override
+    public String toString() {
+        return "Boat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", boatType=" + boatType +
+                '}';
+    }
+}

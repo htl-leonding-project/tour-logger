f71ea5320ee6e4e02aed2a9bbca458731f1dd4bb
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:45:18 +0100

    #12 added delete method CrewMemberRepository.java

--- src/main/java/at/htl/control/CrewMemberRepository.java
+++ src/main/java/at/htl/control/CrewMemberRepository.java
@@ -1,11 +1,20 @@
 package at.htl.control;
 
+import at.htl.model.BoatType;
+import at.htl.model.CrewMember;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class CrewMemberRepository {
 
     @Inject
     EntityManager em;
+
+    @Transactional
+    public void delete(CrewMember crewMember) {
+        em.remove(crewMember);
+    }
 
 }

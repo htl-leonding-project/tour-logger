813f194538372133baf84c96f51ebd0dc0794fba
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:54:29 +0100

    #12 added findAll method PersonRepository.java

--- src/main/java/at/htl/control/PersonRepository.java
+++ src/main/java/at/htl/control/PersonRepository.java
@@ -6,6 +6,7 @@ import at.htl.model.Person;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 public class PersonRepository {
 
@@ -20,6 +21,12 @@ public class PersonRepository {
     @Transactional
     public void save(Person person){
         em.merge(person);
+    }
+
+    public List<Person> findAll() {
+        return em
+                .createNamedQuery("Person.findAll", Person.class)
+                .getResultList();
     }
 
 }

6f776d627ce58ae186666fdc201c300626139af7
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:48:45 +0100

    #12 added delete method LocationRepository.java

--- src/main/java/at/htl/control/LocationRepository.java
+++ src/main/java/at/htl/control/LocationRepository.java
@@ -1,11 +1,20 @@
 package at.htl.control;
 
+import at.htl.model.BoatState;
+import at.htl.model.Location;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class LocationRepository {
 
     @Inject
     EntityManager em;
+
+    @Transactional
+    public void delete(Location location) {
+        em.remove(location);
+    }
 
 }

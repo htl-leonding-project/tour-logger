93c6f9645445ce076c3839c2ae2524834cbe85f8
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:45:48 +0100

    #12 added save method CrewMemberRepository.java

--- src/main/java/at/htl/control/CrewMemberRepository.java
+++ src/main/java/at/htl/control/CrewMemberRepository.java
@@ -17,4 +17,9 @@ public class CrewMemberRepository {
         em.remove(crewMember);
     }
 
+    @Transactional
+    public void save(CrewMember crewMember){
+        em.merge(crewMember);
+    }
+
 }

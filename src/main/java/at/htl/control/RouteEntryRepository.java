9ebd1a86b24e6f45a450a11da1ccfbe4c9e4c8b4
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:56:49 +0100

    #12 added delete method RouteEntryRepository.java

--- src/main/java/at/htl/control/RouteEntryRepository.java
+++ src/main/java/at/htl/control/RouteEntryRepository.java
@@ -1,14 +1,20 @@
 package at.htl.control;
 
+import at.htl.model.BoatState;
+import at.htl.model.RouteEntry;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class RouteEntryRepository {
 
     @Inject
     EntityManager em;
 
-
-
+    @Transactional
+    public void delete(RouteEntry routeEntry) {
+        em.remove(routeEntry);
+    }
 
 }

ab2d8d25301272d422a8ee22bcb293246aaecfff
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:53:10 +0100

    #12 added delete method PersonRepository.java

--- src/main/java/at/htl/control/PersonRepository.java
+++ src/main/java/at/htl/control/PersonRepository.java
@@ -1,11 +1,22 @@
 package at.htl.control;
 
+import at.htl.model.Location;
+import at.htl.model.Person;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class PersonRepository {
 
     @Inject
     EntityManager em;
+
+    @Transactional
+    public void delete(Person person) {
+        em.remove(person);
+    }
+
+
 
 }

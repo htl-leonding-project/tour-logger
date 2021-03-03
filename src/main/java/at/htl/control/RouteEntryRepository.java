4ae43ef2ac7c5f1b093aa9001e4b2514e5ede39e
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:55:37 +0100

    #12 added RouteEntryRepository.java

---
+++ src/main/java/at/htl/control/RouteEntryRepository.java
@@ -0,0 +1,14 @@
+package at.htl.control;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+public class RouteEntryRepository {
+
+    @Inject
+    EntityManager em;
+
+
+
+
+}

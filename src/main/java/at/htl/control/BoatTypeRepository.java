7a60663c67232a463a53dd45f740b9a7f3d87747
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:37:16 +0100

    #12 added delete method to BoatTypeRepository.java

--- src/main/java/at/htl/control/BoatTypeRepository.java
+++ src/main/java/at/htl/control/BoatTypeRepository.java
@@ -1,11 +1,19 @@
 package at.htl.control;
 
+import at.htl.model.BoatType;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class BoatTypeRepository {
 
     @Inject
     EntityManager em;
+
+    @Transactional
+    public void delete(BoatType boatType) {
+        em.remove(boatType);
+    }
 
 }

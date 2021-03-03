3ccc7945fd5011b68dcf6d9296728cded4bac42c
Tobert <tobiashoefl@gmail.com>
2021-03-03 20:59:58 +0100

    #12 added findAll method RouteEntryRepository.java

--- src/main/java/at/htl/control/RouteEntryRepository.java
+++ src/main/java/at/htl/control/RouteEntryRepository.java
@@ -2,10 +2,12 @@ package at.htl.control;
 
 import at.htl.model.BoatState;
 import at.htl.model.RouteEntry;
+import io.vertx.ext.web.Route;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 public class RouteEntryRepository {
 
@@ -20,6 +22,12 @@ public class RouteEntryRepository {
     @Transactional
     public void save(RouteEntry routeEntry){
         em.merge(routeEntry);
+    }
+
+    public List<RouteEntry> findAll() {
+        return em
+                .createNamedQuery("RouteEntry.findAll", RouteEntry.class)
+                .getResultList();
     }
 
 }

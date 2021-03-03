a048ab36e9cbd7f48e695af6c0cf4c22562704b7
Tobert <tobiashoefl@gmail.com>
2021-03-03 21:13:14 +0100

    #12 added delete method TourStateRepository.java

--- src/main/java/at/htl/control/TourStateRepository.java
+++ src/main/java/at/htl/control/TourStateRepository.java
@@ -1,11 +1,22 @@
 package at.htl.control;
 
+import at.htl.model.Tour;
+import at.htl.model.TourState;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 public class TourStateRepository {
 
     @Inject
     EntityManager em;
+
+    @Transactional
+    public void delete(TourState tourState) {
+        em.remove(tourState);
+    }
+
+
 
 }

44106d7770773b1a8495b14bc5bdf23f40681be7
Meris Besic <besicmeris@gmail.com>
2021-10-05 10:51:05 +0200

    added ressource

--- src/main/java/at/htl/boundary/TourRessource.java
+++ src/main/java/at/htl/boundary/TourRessource.java
@@ -1,2 +1,50 @@
-package at.htl.boundary;public class TourRessource {
+package at.htl.boundary;
+
+import at.htl.model.Tour;
+import at.htl.persistance.TourRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.List;
+
+@Path("/api")
+@ApplicationScoped
+public class TourRessource {
+
+    @Inject
+    TourRepository tr;
+
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Tour> getAllTours(){
+        return tr.listAll();
+    }
+
+    @GET
+    @Path("/count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public long countTours(){
+        return tr.count();
+    }
+
+    @POST
+    @Transactional
+    @Path("/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(Tour tour, @Context UriInfo info) {
+        tr.persist(tour);
+        return Response.
+                created(URI.create(info.getPath() + "/" + tour.getId()))
+                .build();
+    }
+
 }
--- src/main/java/at/htl/persistance/TourRepository.java
+++ src/main/java/at/htl/persistance/TourRepository.java
@@ -3,5 +3,8 @@ package at.htl.persistance;
 import at.htl.model.Tour;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class TourRepository implements PanacheRepository<Tour> {
 }
